d9fedf650abdd19d836aa777f14a91a5ab651a13
Almási István Gergely <almasi.istvan.gergely@gmail.com>
2023-09-03 19:39:49 +0200

    products added with img urls

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
     API_URL: "http://localhost:3000",
     DB_URI: "mongodb://localhost:27017/starter-shop"
   },
+  images: {
+    domains: ['www.platinet.pl'],
+  },
 };
 
 module.exports = nextConfig;
