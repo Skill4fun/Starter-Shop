018d249447e2ddf0bd4b154a1e5bfd7985927870
István Gergely Almási <almasi.istvan.gergely@gmail.com>
2023-09-05 14:16:49 +0200

    catageory filter and ratings filter checkboxes implemented client side

--- components/layouts/Filters.js
+++ components/layouts/Filters.js
@@ -2,11 +2,41 @@
 
 import React from "react";
 import StarRatings from "react-star-ratings";
+import { useRouter } from 'next/navigation';
 
 const Filters = () => {
+  const router = useRouter();
+
   let queryParams;
 
-  function checkHandler(checkBoxType, checkBoxValue) {
+  function handleClick(checkbox) {
+    if (typeof window !== "undefined") {
+      queryParams = new URLSearchParams(window.location.search);
+    }
+
+    const checkboxes = document.getElementsByName(checkbox.name);
+
+    checkboxes.forEach((item) => {
+      if (item !== checkbox) item.checked = false;
+    });
+
+    if (checkbox.checked === false) {
+      // Delete the filter from query
+      queryParams.delete(checkbox.name);
+
+    } else {
+      // Set filter in the query
+      if (queryParams.has(checkbox.name)) {
+        queryParams.set(checkbox.name, checkbox.value);
+      } else {
+        queryParams.append(checkbox.name, checkbox.value);
+      }
+    }
+    const path = window.location.pathname + "?" + queryParams.toString();
+    router.push(path);
+  }
+
+  function handleCheck(checkBoxType, checkBoxValue) {
     if (typeof window !== "undefined") {
       queryParams = new URLSearchParams(window.location.search);
     }
@@ -66,7 +96,8 @@ const Filters = () => {
                 type="checkbox"
                 value="Electronics"
                 className="h-4 w-4"
-                defaultChecked={checkHandler("category", "Electronics")}
+                defaultChecked={handleCheck("category", "Electronics")}
+                onClick={(e) => handleClick(e.target)}
               />
               <span className="ml-2 text-gray-500"> Electronics </span>
             </label>
@@ -78,7 +109,8 @@ const Filters = () => {
                 type="checkbox"
                 value="Laptops"
                 className="h-4 w-4"
-                defaultChecked={checkHandler("category", "Laptops")}
+                defaultChecked={handleCheck("category", "Laptops")}
+                onClick={(e) => handleClick(e.target)}
               />
               <span className="ml-2 text-gray-500"> Laptops </span>
             </label>
@@ -90,7 +122,8 @@ const Filters = () => {
                 type="checkbox"
                 value="Toys"
                 className="h-4 w-4"
-                defaultChecked={checkHandler("category", "Toys")}
+                defaultChecked={handleCheck("category", "Toys")}
+                onClick={(e) => handleClick(e.target)}
               />
               <span className="ml-2 text-gray-500"> Toys </span>
             </label>
@@ -102,7 +135,8 @@ const Filters = () => {
                 type="checkbox"
                 value="Office"
                 className="h-4 w-4"
-                defaultChecked={checkHandler("category", "Office")}
+                defaultChecked={handleCheck("category", "Office")}
+                onClick={(e) => handleClick(e.target)}
               />
               <span className="ml-2 text-gray-500"> Office </span>
             </label>
@@ -114,7 +148,8 @@ const Filters = () => {
                 type="checkbox"
                 value="Beauty"
                 className="h-4 w-4"
-                defaultChecked={checkHandler("category", "Beauty")}
+                defaultChecked={handleCheck("category", "Beauty")}
+                onClick={(e) => handleClick(e.target)}
               />
               <span className="ml-2 text-gray-500"> Beauty </span>
             </label>
@@ -133,7 +168,8 @@ const Filters = () => {
                   type="checkbox"
                   value={rating}
                   className="h-4 w-4"
-                  defaultChecked={checkHandler("ratings", `${rating}`)}
+                  defaultChecked={handleCheck("ratings", `${rating}`)}
+                  onClick={(e) => handleClick(e.target)}
                 />
                 <span className="ml-2 text-gray-500">
                   {" "}
